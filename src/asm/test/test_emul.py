be3b4d16b6b617989c6110f9362fe614e76df0fa
eyalrr2 <eyalrr2@gmail.com>
2025-05-10 12:33:24 +0300

    Finish environment

--- src/asm/test/test_emul.py
+++ src/asm/test/test_emul.py
@@ -9,9 +9,9 @@ from unicorn.riscv_const import *
 CODE_ADDRESS = 0
 
 # Memory address where emulations
-RETURN_ADDRESS = 0xc
+RETURN_ADDRESS = 0x10000
 
-@pytest.mark.parametrize("test_input", [(1, 2, 3)])
+@pytest.mark.parametrize("test_input", [(1, 2, 3), (5, 6, 11)])
 def test_emulation(mu: Uc, test_input: tuple):
     mu.reg_write(UC_RISCV_REG_A0, test_input[0])
     mu.reg_write(UC_RISCV_REG_A1, test_input[1])
